a5902776a3d48997620314d56ef788996ed841c2
kodera123 <b.b.015.k.k@softbank.ne.jp>
2016-10-30 14:55:24 +0900

    add precompile icon

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Follow
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
+    config.assets.precompile += %w(*.eot *.svg *.ttf *.woff)
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
-  config.action_controller.asset_host = '//s3-ap-northeast-1.amazonaws.com/follow_sample'
+  config.action_controller.asset_host = '//s3-ap-northeast-1.amazonaws.com/follow-sample'
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
